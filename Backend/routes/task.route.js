c19c65fd45f5ddecb85d2a51d3808173ffb635ac
Balaji Laygude <balajiwork05@gmail.com>
2026-09-09 22:24:06 +0530

    Update task.route.js

--- Backend/routes/task.route.js
+++ Backend/routes/task.route.js
@@ -4,7 +4,7 @@ const { getAllTask, createTask, deletetask, updateTask } = require("../controlle
 const taskRouter=express.Router()
 
 taskRouter.get("/",getAllTask)
-taskRouter.post("/",createTask)
+taskRouter.post("/task",createTask)
 taskRouter.delete("/task/:id",deletetask)
 taskRouter.put("/task/:id",updateTask)
 
